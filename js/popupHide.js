ccde834b9fefb0b0fee5f3b9907cc335b4ac3b16
Daria Plotnikova <etterwin.yll@gmail.com>
2017-10-03 12:13:07 +0300

    update js

--- js/popupHide.js
+++ js/popupHide.js
@@ -4,6 +4,10 @@ $(function($){
         if (!div.is(e.target)
             && div.has(e.target).length === 0) {
             $('#newtask_popup').removeClass('creator-newtask__popup--active');
+            $('#main_popup').removeClass('popup-hide');
+            $('#popup_translator').removeClass('popup-show');
+            $('#popup_developer').removeClass('popup-show');
+            $('#popup_developer').removeClass('popup-show');
         }
     });
 });
